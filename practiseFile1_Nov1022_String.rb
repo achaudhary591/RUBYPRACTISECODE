1d8a773889ad5e5f6fe01b46c335ed50dda4b23b
w3villa-akshya <akshya.chaudhary@w3villa.com>
2022-11-10 15:59:13 +0530

    string interpolation and concatenation

---
+++ practiseFile1_Nov1022_String.rb
@@ -0,0 +1,9 @@
+first_name = "Akshay"
+last_name = "Chaudhary"
+
+#string concat
+puts first_name + " " +last_name
+
+#string interpolation
+
+puts "My first name is #{first_name} and my last name is #{last_name}"
