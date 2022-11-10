efd64a0db23bfe3f8e3feecb088726fa8ea2e821
w3villa-akshya <akshya.chaudhary@w3villa.com>
2022-11-10 23:07:29 +0530

    reversing string

--- practiseFile1_Nov1022_String.rb
+++ practiseFile1_Nov1022_String.rb
@@ -1,6 +1,6 @@
 first_name = "Akshay"
 last_name = "Chaudhary"
-
+full_name = first_name + " " + last_name
 #string concat
 puts first_name + " " +last_name
 
@@ -9,3 +9,8 @@ puts first_name + " " +last_name
 puts "My first name is #{first_name} and my last name is #{last_name}"
 
 #string interpolation works only with double quotes not allowed with single quotes (ruby on rails)
+
+
+puts first_name.reverse
+
+puts full_name.reverse
