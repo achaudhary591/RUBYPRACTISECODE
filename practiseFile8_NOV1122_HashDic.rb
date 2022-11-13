69d3441b6c52ab315eaaddffd18b386e8af1fa28
w3villa-akshya <akshya.chaudhary@w3villa.com>
2022-11-13 20:47:33 +0530

    getting all hashKeys

--- practiseFile8_NOV1122_HashDic.rb
+++ practiseFile8_NOV1122_HashDic.rb
@@ -4,3 +4,12 @@ my_details = {'name'  => 'Akshay', 'favcolor' => 'red'}
 p sample_hash['d']
 
 
+# symbol
+
+another_hash = {a: 1, b: 2, c: 3, d: 4}
+
+p another_hash
+
+# getting only keys from hash
+
+p another_hash.keys
