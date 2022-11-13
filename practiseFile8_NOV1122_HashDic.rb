562f4bd881b44eaf0cbe6f2681936bf186cb1ad8
w3villa-akshya <akshya.chaudhary@w3villa.com>
2022-11-13 20:54:36 +0530

    getting class of key and value

--- practiseFile8_NOV1122_HashDic.rb
+++ practiseFile8_NOV1122_HashDic.rb
@@ -13,3 +13,9 @@ p another_hash
 # getting only keys from hash
 
 p another_hash.keys
+
+#  short snippet to determine datatype of key and value
+
+another_hash.each do |key, value|
+  puts "The class for key is: #{key.class} and for  value is: #{value.class}"
+end
