4309811c16f42e4be639b341b83992f5701930b5
w3villa-akshya <akshya.chaudhary@w3villa.com>
2022-11-14 12:24:05 +0530

    initailiser

--- oops_project/student.rb
+++ oops_project/student.rb
@@ -1,14 +1,17 @@
 class Student
   #attribute accessors annotation(both getter and setter)
-  attr_accessor :first_name, :last_name, :email, :username
+  attr_accessor :first_name, :last_name, :email, :username, :password
 
   # for only getter we have to use attr_reader instead!
 
-  @first_name
-  @last_name
-  @email
-  @username
-  @password
+  # initialise method or constructor
+  def initialize(firstname, lastname,username, email, password)
+    @first_name = firstname
+    @last_name = lastname
+    @username = username
+    @email = email
+    @password = password
+  end
 
   # setter
   # def first_name=(name)
@@ -29,22 +32,37 @@ class Student
   # end
 
   # toString Method
-  # def to_s
-  #   "First Name: #{@first_name}"
-  # end
+  def to_s
+   "    First name: #{@first_name},
+    Last name: #{@last_name},
+    Em@il address: #{@email},
+    Username: #{@username},
+    Password: #{@password},
+    ======================="
+  end
 
 end
 
-akshay = Student.new
+akshay = Student.new("Akshay", "Chaudhary", "chaudhary.akshay", "chaudhary@akshay.com", "akshay")
+shantanu = akshay = Student.new("Shantanu", "Yadav", "yadav.shantanu", "yadav@shantanu.com", "shantanu")
 
 # setting up name
+=begin
 akshay.first_name = "Akshay"
 akshay.last_name = "Chaudhary"
 akshay.email = "chaudhary@akshay.com"
 akshay.username = "chaudhary.akshay"
+=end
 
+=begin
 #getting
 puts "First name: #{akshay.first_name}"
 puts "Last name: #{akshay.last_name}"
 puts "Em@il: #{akshay.email}"
 puts "Username: #{akshay.username}"
+puts "Password: #{akshay.password}"
+=end
+
+puts akshay
+puts shantanu
+
